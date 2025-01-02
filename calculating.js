905b06433bbc9450b38f7a663959ba411c8f2c0c
박서현 <s24005@gsm.hs.kr>
2025-01-02 23:25:50 +0900

    계산기 1일차

---
+++ calculating.js
@@ -0,0 +1,19 @@
+const calculator={
+  add:function(a,b){
+    return a+b;
+  },
+  minus:function(a,b){
+    return a-b;
+  },
+  devide:function(a,b){
+    return a/b;
+  },
+  power:function(a,b){
+    return a**b;
+  }
+  }
+  
+  const plusResult=calculator.add(1,2);
+  
+  
+  console.log(plusResult);
